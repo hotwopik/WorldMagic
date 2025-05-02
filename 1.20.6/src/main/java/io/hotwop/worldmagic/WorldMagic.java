7f1fef5bff66c42e512e08a3ca221e35f25d6819
hotwopik <none>
2025-05-02 21:37:37 +0500

    First commit

---
+++ 1.20.6/src/main/java/io/hotwop/worldmagic/WorldMagic.java
@@ -0,0 +1,16 @@
+package io.hotwop.worldmagic;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WorldMagic extends JavaPlugin {
+    @Override
+    public void onEnable(){
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
